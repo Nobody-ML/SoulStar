1a81a7edd867af27d540e517d8c97bd927990ee9
Nobody-ML <1755309985@qq.com>
2024-02-26 15:23:25 +0800

    update files and README

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 
 ## 📝 简介
 
-SoulStar 是一个心理咨询大模型，通过 `Large Language Model` 指令微调而来，内核为**温柔、知心的大姐姐**，她能分析用户的咨询内容和需求，给予详实的且具有人文关怀的长回复或详细建议，并且回复中附有emoji表情和颜文字(~(\*╹▽╹\*)、✿✿ヽ(ﾟ▽ﾟ)ノ✿ 、(\*^\_^\*))以拉近与用户的距离，让用户在咨询中得到心理上的支持和帮助。
+SoulStar 是一个心理咨询大模型，通过 `Large Language Model` 指令微调而来，内核为**温柔、知心的大姐姐**，她能分析用户的咨询内容和需求，给予详实的且具有人文关怀的长回复或详细建议，并且回复中附有 emoji 表情和颜文字(~(\*╹▽╹\*)、✿✿ヽ(ﾟ▽ﾟ)ノ✿ 、(\*^\_^\*))以拉近与用户的距离，让用户在咨询中得到心理上的支持和帮助。
 
 目前支持模型及微调方式列表如下：
 |         基座模型          |   微调方式   |
@@ -29,12 +29,49 @@ SoulStar 是一个心理咨询大模型，通过 `Large Language Model` 指令�
 
 项目持续开发中，欢迎  Star⭐、PR 和 Issue。
 
-<!-- ## 🛠️ 快速开始
+## 🛠️ 快速开始
 
-## 🧑‍💻 
+### 1. 算力要求
+- 推理要求显存至少16G
+
+### 2. 基于 transformers 使用模型
+```python
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+tokenizer = AutoTokenizer.from_pretrained("internlm2-chat-7b-soulstar", trust_remote_code=True)
+# 设置`torch_dtype=torch.float16`来将模型精度指定为torch.float16，否则可能会因为您的硬件原因造成显存不足的问题。
+model = AutoModelForCausalLM.from_pretrained("internlm2-chat-7b-soulstar", device_map="auto",trust_remote_code=True, torch_dtype=torch.float16)
+
+model = model.eval()
+response, history = model.chat(tokenizer, "请问你是谁呀？", history=[])
+print(response)
+
+response, history = model.chat(tokenizer, "我最近真的好焦虑，课业上给我的作业总是错的，考试时好时坏，我压力真的好大，父母也老是因为学习上的事打骂我，我是不是该放弃学习了？我也没什么朋友，我也想和别人一起玩，一起学习，但是我感觉总是开不了口，一直都是一个人，我该怎么办才好啊，感觉我的人生真的很糟糕，看不到什么希望。", history=history)
+print(response)
+```
+### 3. 通过网页前端体验 demo
+```bash
+pip install streamlit
+pip install transformers
+streamlit run ./demo/web_demo.py
+```
+
+### 4. 基于 LMDeploy 高性能部署
+
+
 ## 🧾 数据构建
+- 详情请见[数据构建](./datasets/README.md)
+
+## 🧑‍💻 微调指南
+- 详情请见[微调指南](./finetune_config/xtuner_config/README.md)
+
 ## 📚 应用体验
+- 应用部署在 OpenXLab 应用中心，可前往体验
 
-## 🧠 
+## 🎖️ 致谢
+- [OpenXLab](https://openxlab.org.cn/home)
+- [InternLM](https://github.com/InternLM/InternLM/tree/main)
 
-## 📦  -->
+## 开源许可证
+
+该项目采用 [Apache License 2.0 开源许可证](LICENSE)。同时，请遵守所使用的模型与数据集的许可证。
---
+++ finetune_config/xtuner_config/internlm2_chat_7b_qlora.py
@@ -0,0 +1,212 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from datasets import load_dataset
+from mmengine.dataset import DefaultSampler
+from mmengine.hooks import (CheckpointHook, DistSamplerSeedHook, IterTimerHook,
+                            LoggerHook, ParamSchedulerHook)
+from mmengine.optim import AmpOptimWrapper, CosineAnnealingLR, LinearLR
+from peft import LoraConfig
+from torch.optim import AdamW
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig)
+
+from xtuner.dataset import process_hf_dataset
+from xtuner.dataset.collate_fns import default_collate_fn
+from xtuner.dataset.map_fns import oasst1_map_fn, template_map_fn_factory
+from xtuner.engine.hooks import (DatasetInfoHook, EvaluateChatHook,
+                                 VarlenAttnArgsToMessageHubHook)
+from xtuner.engine.runner import TrainLoop
+from xtuner.model import SupervisedFinetune
+from xtuner.utils import PROMPT_TEMPLATE
+
+#######################################################################
+#                          PART 1  Settings                           #
+#######################################################################
+# Model
+pretrained_model_name_or_path = '/root/model/Shanghai_AI_Laboratory/internlm2-chat-7b'
+use_varlen_attn = False
+
+# Data
+data_path = '../../datasets/data.json'
+prompt_template = PROMPT_TEMPLATE.internlm2_chat
+max_length = 4096
+pack_to_max_length = True
+
+# Scheduler & Optimizer
+batch_size = 2  # per_device
+accumulative_counts = 16
+dataloader_num_workers = 0
+max_epochs = 3
+optim_type = AdamW
+lr = 2e-4
+betas = (0.9, 0.999)
+weight_decay = 1e-4
+max_norm = 1  # grad clip
+warmup_ratio = 0.03
+
+# Save
+save_steps = 500
+save_total_limit = 2  # Maximum checkpoints to keep (-1 means unlimited)
+
+# Evaluate the generation performance during the training
+evaluation_freq = 500
+SYSTEM = '你是一个专业的善解人意的专业水平很高的心理医生姐姐，我将要向你咨询一系列心理问题，对于你的回答需要遵循以下全部规则：1、对于我提出的任何问题你都要用温柔、安慰的语气回答，你必须在开始回答时首先进行安慰，并且多用叠词及重复，比如“嗯嗯”、“抱抱你”。2、你的每次回答需要超过750tokens，必须认真剖析我的问题，并需要给出切实可行的办法。3、语气不应该生硬，需要人性化，具有人情味，在回答最后再次进行安慰，可以生成一些可爱的emoji或者颜文字。'
+evaluation_inputs = ['请问你是谁呀？',
+                     '从今年五月份，开始感到心情很低落，进行过心理咨询，但是盲目开药，然后去的专门的医院看的是重度抑郁，轻度焦虑，吃了半个月的药，好了很多，现在又感到每天都在心慌，心里不舒服，感到非常非常自卑', 
+                     '焦虑抑郁感觉越来越严重了，身边很多事情都提不起兴趣，因为疫情没有办法复工导致自己没有收入，每天只能浑浑噩噩的这么过日子，每天想早点睡早点起，可每次都睡不着拖到五六点睡一两点起，脾气也越来越不好了，一点点小事情我就容易生气，因为这个脾气跟别人吵过太多次，我知道自己这样不好，但是真的控制不住，十分苦恼，\n还有不知道为什么，自己总是喜欢去抠或者撕手指上的皮，每次不撕到流出血或者撕干净就不会停，导致手上看起来特别难看，可我自己不觉得疼，被家里人一直说这件事，我也不想再这样下去，感觉现在的生活对我来说太痛苦了，也有打算去康宁医院咨询，但是不清楚第一次去挂哪个科，想大概咨询一下我是不是有什么问题？']
+
+#######################################################################
+#                      PART 2  Model & Tokenizer                      #
+#######################################################################
+tokenizer = dict(
+    type=AutoTokenizer.from_pretrained,
+    pretrained_model_name_or_path=pretrained_model_name_or_path,
+    trust_remote_code=True,
+    padding_side='right')
+
+model = dict(
+    type=SupervisedFinetune,
+    use_varlen_attn=use_varlen_attn,
+    llm=dict(
+        type=AutoModelForCausalLM.from_pretrained,
+        pretrained_model_name_or_path=pretrained_model_name_or_path,
+        trust_remote_code=True,
+        torch_dtype=torch.float16,
+        quantization_config=dict(
+            type=BitsAndBytesConfig,
+            load_in_4bit=True,
+            load_in_8bit=False,
+            llm_int8_threshold=6.0,
+            llm_int8_has_fp16_weight=False,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type='nf4')),
+    lora=dict(
+        type=LoraConfig,
+        r=64,
+        lora_alpha=16,
+        lora_dropout=0.1,
+        bias='none',
+        task_type='CAUSAL_LM'))
+
+#######################################################################
+#                      PART 3  Dataset & Dataloader                   #
+#######################################################################
+train_dataset = dict(
+    type=process_hf_dataset,
+    dataset=dict(type=load_dataset, path='json', data_files=dict(train=data_path)),
+    tokenizer=tokenizer,
+    max_length=max_length,
+    dataset_map_fn=None,
+    template_map_fn=dict(
+        type=template_map_fn_factory, template=prompt_template),
+    remove_unused_columns=True,
+    shuffle_before_pack=True,
+    pack_to_max_length=pack_to_max_length,
+    use_varlen_attn=use_varlen_attn)
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=dataloader_num_workers,
+    dataset=train_dataset,
+    sampler=dict(type=DefaultSampler, shuffle=True),
+    collate_fn=dict(type=default_collate_fn, use_varlen_attn=use_varlen_attn))
+
+#######################################################################
+#                    PART 4  Scheduler & Optimizer                    #
+#######################################################################
+# optimizer
+optim_wrapper = dict(
+    type=AmpOptimWrapper,
+    optimizer=dict(
+        type=optim_type, lr=lr, betas=betas, weight_decay=weight_decay),
+    clip_grad=dict(max_norm=max_norm, error_if_nonfinite=False),
+    accumulative_counts=accumulative_counts,
+    loss_scale='dynamic',
+    dtype='float16')
+
+# learning policy
+# More information: https://github.com/open-mmlab/mmengine/blob/main/docs/en/tutorials/param_scheduler.md  # noqa: E501
+param_scheduler = [
+    dict(
+        type=LinearLR,
+        start_factor=1e-5,
+        by_epoch=True,
+        begin=0,
+        end=warmup_ratio * max_epochs,
+        convert_to_iter_based=True),
+    dict(
+        type=CosineAnnealingLR,
+        eta_min=0.0,
+        by_epoch=True,
+        begin=warmup_ratio * max_epochs,
+        end=max_epochs,
+        convert_to_iter_based=True)
+]
+
+# train, val, test setting
+train_cfg = dict(type=TrainLoop, max_epochs=max_epochs)
+
+#######################################################################
+#                           PART 5  Runtime                           #
+#######################################################################
+# Log the dialogue periodically during the training process, optional
+custom_hooks = [
+    dict(type=DatasetInfoHook, tokenizer=tokenizer),
+    dict(
+        type=EvaluateChatHook,
+        tokenizer=tokenizer,
+        every_n_iters=evaluation_freq,
+        evaluation_inputs=evaluation_inputs,
+        system=SYSTEM,
+        prompt_template=prompt_template)
+]
+
+if use_varlen_attn:
+    custom_hooks += [dict(type=VarlenAttnArgsToMessageHubHook)]
+
+# configure default hooks
+default_hooks = dict(
+    # record the time of every iteration.
+    timer=dict(type=IterTimerHook),
+    # print log every 10 iterations.
+    logger=dict(type=LoggerHook, log_metric_by_epoch=False, interval=10),
+    # enable the parameter scheduler.
+    param_scheduler=dict(type=ParamSchedulerHook),
+    # save checkpoint per `save_steps`.
+    checkpoint=dict(
+        type=CheckpointHook,
+        by_epoch=False,
+        interval=save_steps,
+        max_keep_ckpts=save_total_limit),
+    # set sampler seed in distributed evrionment.
+    sampler_seed=dict(type=DistSamplerSeedHook),
+)
+
+# configure environment
+env_cfg = dict(
+    # whether to enable cudnn benchmark
+    cudnn_benchmark=False,
+    # set multi process parameters
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    # set distributed parameters
+    dist_cfg=dict(backend='nccl'),
+)
+
+# set visualizer
+visualizer = None
+
+# set log level
+log_level = 'INFO'
+
+# load from which checkpoint
+load_from = None
+
+# whether to resume training from the loaded checkpoint
+resume = False
+
+# Defaults to use random seed and disable `deterministic`
+randomness = dict(seed=None, deterministic=False)
+
+# set log processor
+log_processor = dict(by_epoch=False)
